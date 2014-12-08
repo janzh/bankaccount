1f662f2ba8d7fbc150f3e35bf604910f27e5f22b
janzh <erikgunnar.jansen@hotmail.com>
2014-12-08 03:44:25 +0100

    jar files

--- src/bankaccount/Main.java
+++ src/bankaccount/Main.java
@@ -7,5 +7,21 @@ public class Main {
 		Replica r = new Replica("54.148.157.54", 8001, 0);
 		GCLI cli = new GCLI(r);
 		r.setListener(cli);
+		
+//		Replica r = new Replica("54.69.100.172", 8001, 1);
+//		GCLI cli = new GCLI(r);
+//		r.setListener(cli);
+//		
+//		Replica r = new Replica("54.149.12.69", 8001, 2);
+//		GCLI cli = new GCLI(r);
+//		r.setListener(cli);
+//		
+//		Replica r = new Replica("54.69.199.175", 8001, 3);
+//		GCLI cli = new GCLI(r);
+//		r.setListener(cli);
+//		
+//		Replica r = new Replica("54.148.74.72", 8001, 4);
+//		GCLI cli = new GCLI(r);
+//		r.setListener(cli);
 	}
 }
